cfd1408a42636e1861d7a24f662ca3e402321811
abdalladiaa <abdallazx009@gmail.com>
2026-04-06 22:32:14 +0200

    fix cart 9

--- src/utils/getDecodedTokenFunc.ts
+++ src/utils/getDecodedTokenFunc.ts
@@ -1,59 +1,42 @@
-// "use server";
-
-// import { decode } from "next-auth/jwt";
-// import { cookies } from "next/headers";
-
-// export async function getDecodedTokenFunc() {
-//   const cookieStore = await cookies();
-
-//   const cookieName =
-//     process.env.NODE_ENV === "production"
-//       ? "__Secure-next-auth.session-token"
-//       : "next-auth.session-token";
-
-//   let token = cookieStore.get(cookieName)?.value;
-
-//   if (!token) {
-//     let chunk = "";
-//     let i = 0;
-//     while ((chunk = cookieStore.get(`${cookieName}.${i}`)?.value ?? "")) {
-//       token = (token ?? "") + chunk;
-//       i++;
-//     }
-//   }
-
-//   console.log("TOKEN:", token);
-
-//   if (!token) return null;
-
-//   const decodedCookie = await decode({
-//     secret: process.env.AUTH_SECRET!,
-//     token,
-//   });
-
-//   console.log("Decoded", decodedCookie?.userToken);
-
-//   return decodedCookie?.userToken;
-// }
-
-// const token =
-//   cookieStore.get("__Secure-next-auth.session-token")?.value ||
-//   cookieStore.get("next-auth.session-token")?.value;
-
 "use server";
 
 import { decode } from "next-auth/jwt";
 import { cookies } from "next/headers";
 
-
 export async function getDecodedTokenFunc() {
   const cookieStore = await cookies();
-  const token =
-    cookieStore.get("next-auth.session-token")?.value ||
-    cookieStore.get("__Secure-next-auth.session-token")?.value;
+
+  const cookieName =
+    process.env.NODE_ENV === "production"
+      ? "__Secure-next-auth.session-token"
+      : "next-auth.session-token";
+
+
+  let token = cookieStore.get(cookieName)?.value;
+
+  if (!token) {
+    let chunk = "";
+    let i = 0;
+    while ((chunk = cookieStore.get(`${cookieName}.${i}`)?.value ?? "")) {
+      token = (token ?? "") + chunk;
+      i++;
+    }
+  }
+
+  console.log("TOKEN:", token);
+
+  if (!token) return null;
+
   const decodedCookie = await decode({
+    secret: process.env.AUTH_SECRET!,
     token,
-    secret: process.env.NEXTAUTH_SECRET!,
   });
+
+  console.log("Decoded", decodedCookie?.userToken);
+
   return decodedCookie?.userToken;
 }
+
+  // const token =
+  //   cookieStore.get("__Secure-next-auth.session-token")?.value ||
+  //   cookieStore.get("next-auth.session-token")?.value;
