46faf7d018cb0ebd31723c2d5e7bdda1e36e97a0
abdalladiaa <abdallazx009@gmail.com>
2026-04-06 22:54:03 +0200

    fix cart 10

--- src/utils/getDecodedTokenFunc.ts
+++ src/utils/getDecodedTokenFunc.ts
@@ -6,22 +6,9 @@ import { cookies } from "next/headers";
 export async function getDecodedTokenFunc() {
   const cookieStore = await cookies();
 
-  const cookieName =
-    process.env.NODE_ENV === "production"
-      ? "__Secure-next-auth.session-token"
-      : "next-auth.session-token";
-
-
-  let token = cookieStore.get(cookieName)?.value;
-
-  if (!token) {
-    let chunk = "";
-    let i = 0;
-    while ((chunk = cookieStore.get(`${cookieName}.${i}`)?.value ?? "")) {
-      token = (token ?? "") + chunk;
-      i++;
-    }
-  }
+  const token =
+    cookieStore.get("__Secure-next-auth.session-token")?.value ||
+    cookieStore.get("next-auth.session-token")?.value;
 
   console.log("TOKEN:", token);
 
@@ -30,6 +17,11 @@ export async function getDecodedTokenFunc() {
   const decodedCookie = await decode({
     secret: process.env.AUTH_SECRET!,
     token,
+    // Vercel changes the salt during encryption for secure cookies, so we need to match it here
+    salt:
+      process.env.NODE_ENV === "production"
+        ? "__Secure-next-auth.session-token"
+        : "next-auth.session-token",
   });
 
   console.log("Decoded", decodedCookie?.userToken);
@@ -37,6 +29,6 @@ export async function getDecodedTokenFunc() {
   return decodedCookie?.userToken;
 }
 
-  // const token =
-  //   cookieStore.get("__Secure-next-auth.session-token")?.value ||
-  //   cookieStore.get("next-auth.session-token")?.value;
+// const token =
+//   cookieStore.get("__Secure-next-auth.session-token")?.value ||
+//   cookieStore.get("next-auth.session-token")?.value;
